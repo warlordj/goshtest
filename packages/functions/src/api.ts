23e445664c0cc0e97449d08519452d1a8e698131
Jeremy Lee <jeremy@keenpayroll.ca>
2025-01-07 16:52:37 -0330

    simplied the app

--- packages/functions/src/api.ts
+++ packages/functions/src/api.ts
@@ -1,10 +1,8 @@
-import { Resource } from "sst";
 import { Handler } from "aws-lambda";
-import { Example } from "@gosh/core/example";
 
 export const handler: Handler = async (_event) => {
   return {
     statusCode: 200,
-    body: `${Example.hello()} Linked to ${Resource.MyBucket.name}.`,
+    body: `abcdefgh`,
   };
 };
